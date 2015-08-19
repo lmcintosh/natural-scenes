2b56e66aeb394a54b72fd593ccf431c94d0c7255
Lane McIntosh <lanemcintosh@gmail.com>
2015-08-19 14:53:50 -0700

    updated iterable class for natural scenes stimuli, and added notebooks for working with data

--- load_natural_stimulus.py
+++ load_natural_stimulus.py
@@ -1,5 +1,5 @@
-import numpy as np
 from __future__ import division
+import numpy as np
 
 def rescale(x):    
     # note that if python3 division isn't being used, integers will be truncated
@@ -30,23 +30,32 @@ class NaturalScenesStimulus(object):
     '''
 
     def __init__(self, images, stimulus):
-        '''Images should be (num_images, height, width).
+        '''
+        Images should be (num_images, height, width).
         Stimulus should be (num_frames, 3) where the three
-        dimensions refer to image index, xstart, ystart.'''
+        dimensions refer to image index, xstart, ystart.
+        '''
         self.images = images
         self.stimulus = stimulus
         self.ndims = 500
+        self.shape = (stimulus.shape[0], self.ndims, self.ndims)
 
     def __getitem__(self, index):
+        '''
+        Returns np array of shape (index.shape[0], self.ndims, self.ndims)
+        '''
         img_index = self.stimulus[index, 0].astype('int')
         xstart = self.stimulus[index, 1].astype('int')
         ystart = self.stimulus[index, 2].astype('int')
         # Need to check if index is integer or iterable
+        #import pdb
+        #pdb.set_trace()
         try:
             # assume index is a slice
-            imgs = [rescale(self.images[img_idx]) for img_idx in img_index]
+            imgs = [rescale(self.images[img_idx-1]) for img_idx in img_index]
             return np.array([2*img[y:y+self.ndims, x:x+self.ndims] for x,y,img in zip(xstart,ystart,imgs)])
         except:
             # otherwise index is an integer
-            img = rescale(self.images[img_index])
+            img = rescale(self.images[img_index-1])
             return img[ystart:ystart+self.ndims, xstart:xstart+self.ndims]
+
